93f5eae07f0b83488caf2a0503b88b49f2b7215f
rajendrachy <chyrajendra32@gmail.com>
2025-06-30 14:08:51 +0530

    start lac2 challange question

---
+++ PART1/LAC2/FirstProgram.java
@@ -0,0 +1,10 @@
+ import java.lang.*;
+// package PART1.LAC2;
+
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.print("Rajendra Chaudhary");
+  }
+}
+
