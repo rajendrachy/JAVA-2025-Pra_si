04ce7643f5535b578fadc32dc348fae9722e5106
rajendrachy <chyrajendra32@gmail.com>
2025-06-30 14:14:41 +0530

    start practice qn time 2 10min

--- PART1/LAC2/challange.java
+++ PART1/LAC2/challange.java
@@ -1,3 +1,13 @@
+import java.lang.*;
+
 public class challange {
-  
+  public static void main(String[] args) {
+    System.out.print("Good Morning");
+  }
 }
+
+
+
+// ----------Print start in the simple Way :  --------------
+// Link-> System.out.print("*");
+// System.out.print("* *");
