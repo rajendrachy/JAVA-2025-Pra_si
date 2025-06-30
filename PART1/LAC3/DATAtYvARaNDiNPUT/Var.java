fd4fe75e0c9988dc5b35ad7a1e0cd317eeca8d4a
rajendrachy <chyrajendra32@gmail.com>
2025-06-30 19:29:04 +0530

    start lac3 2 38min

---
+++ PART1/LAC3/DATAtYvARaNDiNPUT/Var.java
@@ -0,0 +1,15 @@
+package PART1.LAC3.DATAtYvARaNDiNPUT;
+
+public class Var {
+  public static void main(String[] args) {
+    int myNumber = 10; // Integer variable // Variable decleration and initialization
+    int yourNumber; // Another integer variable // VAriable declaration only
+
+  yourNumber = 20;
+
+  System.out.println("My Number is: " + myNumber); // Print myNumber
+System.out.println("Your Number is: " + yourNumber); // Print yourNumber
+  }
+}
+
+
