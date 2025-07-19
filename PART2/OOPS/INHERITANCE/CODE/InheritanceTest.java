e5523259906081afd3a94dcebb09ec0b006594ab
rajendrachy <chyrajendra32@gmail.com>
2025-07-20 00:34:27 +0530

    inheritance

---
+++ PART2/OOPS/INHERITANCE/CODE/InheritanceTest.java
@@ -0,0 +1,11 @@
+package PART2.OOPS.INHERITANCE.CODE;
+
+public class InheritanceTest {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.commute();
+
+        TwoWheeler bike = new TwoWheeler();
+        bike.commute();
+    }
+}
