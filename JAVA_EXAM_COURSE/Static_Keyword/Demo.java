5e2a85d082159c7aa15bf0d541b0384d2fd76092
rajendrachy <chyrajendra32@gmail.com>
2025-07-22 21:16:49 +0530

    static keyword done

---
+++ JAVA_EXAM_COURSE/Static_Keyword/Demo.java
@@ -0,0 +1,43 @@
+//-----------This is static varaible---------------
+
+class Mobile {
+    String brand; // this are instance variable 
+    static String name; // make this variable common to all the Object
+    static int price;
+
+    //------- Variable creating inside a method are a local Variable----
+
+    // Method to display mobile details
+    void show() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Mobile m = new Mobile();   // Create object
+        m.brand = "Samsung";       // Set brand
+        m.name = "Galaxy S21";     // Set name
+        m.price = 30000;           // Set price
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Oppo";
+        obj2.name = "Readmi12";
+        obj2.price = 20000;
+
+
+        m.name = "Rajendra"; // change in both // the static variable should calleb by the class name Best practice
+
+        Mobile.price = 5000000;
+
+        m.show(); // Show details
+        obj2.show();
+                         
+    }
+}
+
+
+
---
+++ JAVA_EXAM_COURSE/Static_Keyword/StaticMethod.java
@@ -0,0 +1,44 @@
+//-----------This is static Method ---------------
+
+class Mobile {
+    //String brand; // this are instance variable 
+    static String brand;
+    static String name; // make this variable common to all the Object
+    static int price;
+
+    //------- Variable creating inside a method are a local Variable----
+
+   public  void show() { // it is instance method
+        System.out.println("Brand: " + brand);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+
+
+    public static void show1() { // we should use static variable only under static method
+
+        System.out.println("In static method");
+        System.out.println(brand + " " + name + " " + price);
+    }
+}
+
+
+
+public class StaticMethod {
+    public static void main(String[] args) {
+      
+            Mobile.show1();
+    }
+}
+
+
+
+
+// Syntax of Static Block:
+
+// static {
+//     // initialization code
+// }
+
+
+
