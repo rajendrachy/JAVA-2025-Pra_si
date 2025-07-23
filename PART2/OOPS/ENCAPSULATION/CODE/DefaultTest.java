be874724dca1f4b2af4503377900d0df31a7ea08
rajendrachy <chyrajendra32@gmail.com>
2025-07-23 23:29:12 +0530

    header

---
+++ PART2/OOPS/ENCAPSULATION/CODE/DefaultTest.java
@@ -0,0 +1,13 @@
+package PART2.OOPS.ENCAPSULATION.CODE;
+
+public class DefaultTest {
+  public static void main(String[] args) {
+    
+    Car car = new Car("Yellow", "Dezire", 500.0, 3000);
+
+
+  }
+}
+
+
+
