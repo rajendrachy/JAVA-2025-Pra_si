9e35bc760b6dc64db42dab5f4bc0e75acb176774
rajendrachy <chyrajendra32@gmail.com>
2025-08-27 19:11:44 +0530

    add oops concept inheritance

---
+++ OOPS/ENCAPSULATION/CODE/packages.java
@@ -0,0 +1,12 @@
+package PART2.OOPS.ENCAPSULATION.CODE;
+
+import java.util.Scanner;
+public class packages {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+  }
+}
+
+
+
